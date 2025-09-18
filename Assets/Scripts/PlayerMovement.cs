e65c929146b52c4883a8b2f62731df90afa3a66d
AaronJonesDesigns <ajones90747@uat.edu>
2025-09-18 14:23:45 -0700

    Corrections to public variables
    Corrected code to include public variables for X and Y values.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,11 @@ public class PlayerMovement : MonoBehaviour
 {
     // Variable to hold our sprite Transform information
     private Transform tf;
+    // Sets the min and max X and Y values to public, so they can be edited in the Player Movement component
+    public float minX = -8f;
+    public float maxX = 8f;
+    public float minY = -4.5f;
+    public float maxY = -4.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,11 +22,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If the "W" is pushed down this function will run once per frame
         if (Input.GetKeyDown(KeyCode.W))
         {
-            // pick a random x and y value within the specified range (lower half of the play space)
-            float randomX = Random.Range(-4f, 4f);
-            float randomY = Random.Range(-5f, 0f);
+            // pick a random x and y value within the specified range (in the inspector)
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
 
             // set our new position to that random point along X and Y, while keeping Z locked at 0.
             tf.position = new Vector3(randomX, randomY, 0f);
